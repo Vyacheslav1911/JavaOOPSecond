dd82a561b35ac3fcb055a095b182e38ea5e96e11
Vyacheslav1911 <slavagavrilov99@gmail.com>
2022-06-26 16:24:21 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BmiService service = new BmiService();
+
+        double weight = 120;
+        double height = 210;
+        double index = service.calculate(weight, height);
+
+        System.out.println(index);
+    }
+}
